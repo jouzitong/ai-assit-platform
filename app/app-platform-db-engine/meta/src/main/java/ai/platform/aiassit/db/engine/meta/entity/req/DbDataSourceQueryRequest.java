9d3590265bc9438c486d241656ac0d8bfbf581d6
zhouzhitong <528382226@qq.com>
2026-06-12 16:41:55 +0800

    feat: 添加数据源和表元数据相关SQL及HTTP接口
    - 新增 `db_data_source`、`db_table_meta`、`db_table_field_meta` 和 `db_table_index_meta` 表结构
    - 新增 `data-source.http`、

--- app/app-platform-db-engine/meta/src/main/java/ai/platform/aiassit/db/engine/meta/entity/req/DbDataSourceQueryRequest.java
+++ app/app-platform-db-engine/meta/src/main/java/ai/platform/aiassit/db/engine/meta/entity/req/DbDataSourceQueryRequest.java
@@ -3,11 +3,13 @@ package ai.platform.aiassit.db.engine.meta.entity.req;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.athena.framework.data.jdbc.req.BaseRequest;
+import org.athena.framework.data.mybatis.annotations.IgnoredQuery;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class DbDataSourceQueryRequest extends BaseRequest {
 
+    @IgnoredQuery
     private String keyword;
 
     private String sourceKey;
